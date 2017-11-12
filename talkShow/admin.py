07dc0a537dea95392b6a49bf51d1afb7a019da51
luong-hoang <luong.hoang@ceresolutions.com>
2017-11-12 13:54:34 +0700

    Initialize models

--- talkShow/admin.py
+++ talkShow/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Subject, TalkShow, TalkShowSubject
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Subject)
+admin.site.register(TalkShow)
+admin.site.register(TalkShowSubject)
+
--- talkShow/models.py
+++ talkShow/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    display_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.email
+
+
+class TalkShow(models.Model):
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.date
+
+
+class Subject(models.Model):
+    subject = models.CharField(max_length=255)
+    date_added = models.DateTimeField()
+    date_modified = models.DateTimeField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    talk_show_id = models.ForeignKey(TalkShow, on_delete=models.CASCADE, blank=True)
+
+    def __str__(self):
+        return self.subject
+
+
+class TalkShowSubject(models.Model):
+    talk_show_id = models.ForeignKey(TalkShow, on_delete=models.CASCADE)
+    subject_id = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # def __str__(self):
+    #     return TalkShowSubject.objects
+
+# Create your models here.
