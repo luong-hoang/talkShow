6f2e060e7d649f2f11497f0bc5195b28ea37db49
luong-hoang <luong.hoang@ceresolutions.com>
2017-11-12 18:54:30 +0700

    Models: Delete talk_show_subject will leave all others remain

--- talkShow/models.py
+++ talkShow/models.py
@@ -16,7 +16,7 @@ class TalkShow(models.Model):
     date = models.DateField()
 
     def __str__(self):
-        return self.date
+        return str(self.date)
 
 
 class Subject(models.Model):
@@ -31,11 +31,11 @@ class Subject(models.Model):
 
 
 class TalkShowSubject(models.Model):
-    talk_show_id = models.ForeignKey(TalkShow, on_delete=models.CASCADE)
-    subject_id = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    talk_show_id = models.ForeignKey(TalkShow, on_delete=models.DO_NOTHING)
+    subject_id = models.ForeignKey(Subject, on_delete=models.DO_NOTHING)
+    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
-    # def __str__(self):
-    #     return TalkShowSubject.objects
+    def __str__(self):
+        return '%s: %s talked %s' % (str(self.talk_show_id.date), self.user_id.email, self.subject_id.subject)
 
 # Create your models here.
