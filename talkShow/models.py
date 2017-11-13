c6b487fe3c59241d633e7b1564173a62233c7a65
luong-hoang <luong.hoang@ceresolutions.com>
2017-11-13 16:05:32 +0700

    Models: Remove Subject not remove User

--- talkShow/models.py
+++ talkShow/models.py
@@ -30,7 +30,7 @@ class Subject(models.Model):
     subject = models.CharField(max_length=255)
     date_added = models.DateTimeField()
     date_modified = models.DateTimeField()
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.subject
